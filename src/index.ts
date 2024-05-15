8bfc45a196726121abfb46d2c5677d78443cfd66
Andrew <armcburn@gmail.com>
2024-05-15 07:27:06 -0400

    refactor export name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import formatDate from "./functions/formatDate/formatDate";
 
-const date = new Date("December 17, 1995");
-
-const result = formatDate(date, "ddd-mmm-yyyy");
-console.log(`Formatted date:: ${result}`);
+export function formatDateString(date: Date, format: string) {
+  return formatDate(date, format);
+}
